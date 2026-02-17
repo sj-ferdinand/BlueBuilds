c6bde42450f24a79c7e0b50d191be433d04ddfd3
Sandy F <sandyforrest001@outlook.com>
2026-02-17 12:44:30 +0000

    2026-02-17.2
    * add printer drivers
    * more packages

---
+++ files/scripts/base-build-packagekit-bootc.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+# Your code goes here.
+git clone https://github.com/FyraLabs/PackageKit-bootc.git
+cd PackageKit-bootc
+meson setup builddir
+meson compile -C builddir
+
+sudo meson install -C builddir
