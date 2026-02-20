085933a18a516c45ed28643fe89177a5c038b9de
sj-ferdinand <sandyforrest001@outlook.com>
2026-02-20 11:42:23 +0000

    5i

--- files/scripts/base-add-fstab-extras.sh
+++ files/scripts/base-add-fstab-extras.sh
@@ -5,10 +5,17 @@
 # builds actually ran successfully without any errors!
 set -oue pipefail
 
+echo ====FSTAB BEFORE====
+cat /etc/fstab
+echo ====\n
+
 # Your code goes here.
 cat ./resources/base-fstab >> /etc/fstab
 
+echo ====ADDITIONS====
 cat ./resources/base-fstab
 echo ====\n
+
+echo ====FSTAB AFTER====
 cat /etc/fstab
 echo ====\n
