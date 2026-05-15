80b9c7599680c943b3ed3d60ceb639f1c15b9552
sj-ferdinand <sandyforrest001@outlook.com>
2026-05-15 11:26:44 +0100

    11j
    - better build id

---
+++ files/scripts/better-build-id.sh
@@ -0,0 +1,6 @@
+ogver="$(cat /etc/os-release | grep OSTREE_VERSION | cut -d '"' -f 2)"
+ogbuild="$(cat /etc/os-release | grep BUILD_ID | cut -d '"' -f 2)"
+
+newbuild="$ogver-$ogbuild"
+
+sed -i 's/$ogbuild/$newbuild/g' /etc/os-release
